74d931b43947adb956d73a5269dca6384012f45c
Josh Higginbottom <joshhigginbottom@outlook.com>
2022-03-06 02:00:11 +0000

    Added necessary SQL functionality and started front end work

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {data:[]}
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+      const response = await fetch('/Employees/Get');
+      const json = await response.json();
+      this.setState({data:json});
+  }
 
   render () {
     return (
-      <div>Complete your app here</div>
+        <table className='table table-striped' aria-labelledby="tabelLabel">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Value</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                {this.state.data.map(item =>
+                    <tr>
+                        <td>{item.name}</td>
+                        <td>{item.value}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
     );
   }
 }
--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -10,6 +10,7 @@ namespace InterviewTest.Controllers
     public class EmployeesController : ControllerBase
     {
         [HttpGet]
+        [Route("[action]")]
         public List<Employee> Get()
         {
             var employees = new List<Employee>();
@@ -35,6 +36,63 @@ namespace InterviewTest.Controllers
             }
 
             return employees;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public List<object> GetABC()
+        {
+            var sums = new List<object>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText =      @"select SUBSTR(NAME,1,1), SUM(VALUE)
+                                            from employees
+                                            where SUBSTR(NAME,1,1) in ('A','B','C')
+                                            group by SUBSTR(NAME,1,1)
+                                            having SUM(VALUE) >= 11171";
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        sums.Add(new
+                        {
+                            Letter = reader.GetString(0),
+                            Value = reader.GetInt32(1)
+                        });
+                    }
+                }
+            }
+
+            return sums;
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public bool AddToValue()
+        {
+            var sums = new List<object>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var sqlCmd = connection.CreateCommand();
+                sqlCmd.CommandText = @"UPDATE Employees
+                                         SET Value =
+                                         CASE
+                                         	WHEN UPPER(SUBSTR(Name,1,1)) = 'E' THEN VALUE + 1
+                                         	WHEN UPPER(SUBSTR(Name,1,1)) = 'G' THEN VALUE + 10
+                                         	ELSE VALUE + 100
+                                         END";
+                sqlCmd.ExecuteNonQuery();
+            }
+            return true;
         }
     }
 }
